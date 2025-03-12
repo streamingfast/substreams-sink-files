78f8913d2f710a27a10abb74e161c0db04ccf4c5
Matthieu Vachon <matt@streamingfast.io>
2025-03-12 00:58:58 -0400

    Added more schema tests

--- parquetx/schema_test.go
+++ parquetx/schema_test.go
@@ -54,6 +54,39 @@ func TestSchemaFromMessageDescriptor(t *testing.T) {
                 }
 			`),
 		},
+		{
+			"nested field",
+			(&pbtesting.RowColumnNestedMessage{}).ProtoReflect().Descriptor(),
+			schemaLiteral(`
+            	message rows {
+            	  required group nested {
+            	    required binary value (STRING);
+            	  }
+            	}
+			`),
+		},
+		{
+			"repeated nested field",
+			(&pbtesting.RowColumnRepeatedNestedMessage{}).ProtoReflect().Descriptor(),
+			schemaLiteral(`
+            	message rows {
+            	  repeated group nested {
+            	    required binary value (STRING);
+            	  }
+            	}
+			`),
+		},
+		{
+			"nested has repeated field",
+			(&pbtesting.RowColumnNestedRepeatedMessage{}).ProtoReflect().Descriptor(),
+			schemaLiteral(`
+				message rows {
+				  required group nested {
+				    repeated binary value (STRING);
+				  }
+				}
+			`),
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
